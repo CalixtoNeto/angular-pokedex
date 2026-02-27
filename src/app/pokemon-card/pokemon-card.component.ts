b21dd3799fe7b40ce03d2f35a541567dbd6702ed
CalixtoNeto <36490064+CalixtoNeto@users.noreply.github.com>
2026-02-27 17:48:52 -0300

    chore: prepare Angular v21 upgrade configuration

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -5,6 +5,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
+  standalone: false,
   styleUrls: ['./pokemon-card.component.css']
 })
 export class PokemonCardComponent implements OnInit {
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -6,6 +6,7 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
+  standalone: false,
   styleUrls: ['./pokemon-list.component.css']
 })
 export class PokemonListComponent implements OnInit {
